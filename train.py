93e3ec1c7862c530b55e4507c7eb66ab4625687c
Robert Vacareanu <robert.vacareanu@gmail.com>
2023-03-05 03:07:59 -0700

    Training with NLI

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 from transformers import Seq2SeqTrainer, AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainingArguments, AutoModelForCausalLM, DataCollatorForSeq2Seq, DataCollatorForLanguageModeling, set_seed
 
-from relation_extraction_task import get_prepared_dataset
+from relation_extraction_task import get_prepared_dataset as get_re_dataset
+from nli_task import get_prepared_dataset as get_nli_dataset
+
+import datasets
 
 # Import the W&B Python Library 
 import os
@@ -14,28 +17,32 @@ set_seed(1)
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
 tokenizer.pad_token = tokenizer.eos_token
 model     = AutoModelForCausalLM.from_pretrained("gpt2")
-dataset   = get_prepared_dataset(tokenizer=tokenizer)
+dataset_re   = get_re_dataset(tokenizer=tokenizer)
+dataset_nli  = get_nli_dataset(tokenizer=tokenizer)
+dataset = datasets.interleave_datasets([dataset_re['train'].filter(lambda x: len(x['input_ids']) < 112), dataset_nli['train'].filter(lambda x: len(x['input_ids']) < 112)])
+
+print(dataset)
 
 # collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, max_length=512, padding=True)
-collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False, pad_to_multiple_of=8)
 
 
-output_dir = "output"
+output_dir = "output3"
 
 training_args = Seq2SeqTrainingArguments(
     output_dir                  = output_dir,
-    fp16                        = False,
+    fp16                        = True,
     # fp16_backend                = "amp",
-    per_device_train_batch_size = 32,
-    per_device_eval_batch_size  = 32,
+    per_device_train_batch_size = 64,
+    per_device_eval_batch_size  = 64,
     # eval_accumulation_steps     = 16,
-    evaluation_strategy         = "steps",
-    eval_steps                  = 5000,      #logging_steps,
-    save_steps                  = 5000,
+    # evaluation_strategy         = "steps",
+    # eval_steps                  = 5000,      #logging_steps,
+    save_steps                  = 10000,
     logging_steps               = 50,
-    save_total_limit            = 2,
+    save_total_limit            = 6,
     max_steps                   = 50000,
-    gradient_accumulation_steps = 4,
+    gradient_accumulation_steps = 2,
     report_to                   = "wandb",
     remove_unused_columns       = False,
     # weight_decay                = 0.001,
@@ -43,15 +50,14 @@ training_args = Seq2SeqTrainingArguments(
     lr_scheduler_type           = 'linear',
     dataloader_num_workers      = 16,
     learning_rate               = 3e-4,
-    load_best_model_at_end      = True,
 )
 
 
 trainer = Seq2SeqTrainer(
     model           = model,
     args            = training_args,
-    train_dataset   = dataset['train'].filter(lambda x: len(x['input_ids']) < 96),
-    eval_dataset    = dataset['validation'],
+    train_dataset   = dataset,
+    # eval_dataset    = dataset['validation'],
     # eval_dataset    = {'en_ner': en_ner['validation'].select(range(1000)), 'fr_ner': fr_ner['validation'].select(range(1000))},
     tokenizer       = tokenizer,
     data_collator   = collator,
@@ -59,5 +65,4 @@ trainer = Seq2SeqTrainer(
 )
 
 trainer.train()
-trainer.save_model(output_dir=f'{output_dir}/best')
 
